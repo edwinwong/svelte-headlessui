e60bcd4ab3778c80253c73e123984e4318506728
Edwin Wong <wong.edwin@gmail.com>
2023-03-26 20:14:51 -0400

    Fixing on-click-outside to only include where mousedown and mouseup are outside the element

--- packages/lib/src/lib/internal/on-click-outside.ts
+++ packages/lib/src/lib/internal/on-click-outside.ts
@@ -2,11 +2,21 @@ import type { Behavior } from "./behavior"
 import { listener } from "./events"
 
 export function onClickOutside(fn: (event: Event) => void, when?: () => boolean): Behavior {
+  let mousedownOutside = false
   return node => {
-    function handler(event: Event) {
+    function handleDown(event: Event) {
+      if (!node.contains(event.target as Node)) {
+          mousedownOutside = true
+        }
+      }
+      listener(document.documentElement, 'mousedown', handleDown, true)
+      // Memory leak? Listener not passed anywhere to be removed
+
+    function handleClick(event: Event) {
       if ((event as PointerEvent).pointerType === '') return // ignore space as click
       if (!when || when()) {
-        if (!node.contains(event.target as Node)) {
+        if (!node.contains(event.target as Node) && mousedownOutside) {
+          mousedownOutside = false
           if (node.clientWidth) {
             event.preventDefault()
             event.stopPropagation()
@@ -16,6 +26,6 @@ export function onClickOutside(fn: (event: Event) => void, when?: () => boolean)
       }
     }
 
-    return listener(document.documentElement, 'click', handler, true)
+    return listener(document.documentElement, 'click', handleClick, true)
   }
 }
